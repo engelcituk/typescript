92e8174a1756b6f739df6d72176c60055aa4fad2
cituk <ecituk@thefives.com.mx>
2023-01-26 00:28:47 -0500

    Introducción a los Genéricos

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,13 +1,22 @@
 // import { Hero as SuperHero } from './classes/Hero';
-import * as HeroClasess from './classes/Hero'; //acceso a todo lo que está exportando ese archivo
-import myPowers, { Power } from './data/powers';
+// import * as HeroClasess from './classes/Hero'; //acceso a todo lo que está exportando ese archivo
+// import myPowers, { Power } from './data/powers';
+import { printObject, genericFunction } from './generics/generics';
 
 
 // const iroman = new SuperHero('Ironman', 1, 55)
-const iroman = new HeroClasess.Hero('Ironman', 1, 55)
+// const iroman = new HeroClasess.Hero('Ironman', 1, 55)
 
 
 
-console.log( iroman.power )
-console.log( myPowers )
+// console.log( iroman.power )
+// console.log( myPowers )
+
+// printObject(123)
+// printObject(new Date())
+// printObject({a:1, b:2, c:3})
+// printObject([1,2,3,4,5,6,7,8, 9, 10])
+// printObject('hola mundo')
+
+console.log( genericFunction(3.1416).toFixed(2) )
 
