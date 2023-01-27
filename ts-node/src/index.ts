e2b906f38ee60bd9e1356d70845c51702bc6729d
cituk <ecituk@thefives.com.mx>
2023-01-26 22:57:24 -0500

    Funciones Genéricas

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,7 +1,7 @@
 // import { Hero as SuperHero } from './classes/Hero';
 // import * as HeroClasess from './classes/Hero'; //acceso a todo lo que está exportando ese archivo
 // import myPowers, { Power } from './data/powers';
-import { printObject, genericFunction } from './generics/generics';
+import { printObject, genericFunction, genericFunctionArrow} from './generics/generics';
 
 
 // const iroman = new SuperHero('Ironman', 1, 55)
@@ -18,5 +18,12 @@ import { printObject, genericFunction } from './generics/generics';
 // printObject([1,2,3,4,5,6,7,8, 9, 10])
 // printObject('hola mundo')
 
+const name: string = 'Luceli' 
+
 console.log( genericFunction(3.1416).toFixed(2) )
+console.log( genericFunction(name).toUpperCase() )
+console.log( genericFunction( new Date() ).getDate() )
+
+console.log( genericFunctionArrow('Hola mundo').toUpperCase() )
+
 
