7f8ba77d21c1845d3255cde1ba1151497460c6a8
cituk <ecituk@thefives.com.mx>
2023-01-26 23:22:00 -0500

    Agrupar exportaciones

---
+++ ts-node/src/backs/generics.ts
@@ -0,0 +1,37 @@
+// import { Hero as SuperHero } from './classes/Hero';
+// import * as HeroClasess from './classes/Hero'; //acceso a todo lo que está exportando ese archivo
+// import myPowers, { Power } from './data/powers';
+import { printObject, genericFunction, genericFunctionArrow} from '../generics/generics';
+import { Villain, Hero } from '../interfaces';
+
+
+
+// const iroman = new SuperHero('Ironman', 1, 55)
+// const iroman = new HeroClasess.Hero('Ironman', 1, 55)
+
+
+
+// console.log( iroman.power )
+// console.log( myPowers )
+
+// printObject(123)
+// printObject(new Date())
+// printObject({a:1, b:2, c:3})
+// printObject([1,2,3,4,5,6,7,8, 9, 10])
+// printObject('hola mundo')
+/*
+const name: string = 'Luceli' 
+
+console.log( genericFunction(3.1416).toFixed(2) )
+console.log( genericFunction(name).toUpperCase() )
+console.log( genericFunction( new Date() ).getDate() )
+
+console.log( genericFunctionArrow('Hola mundo').toUpperCase() )*/
+
+const deadPool = {
+    name: 'DeadPool',
+    realName: 'Wade Winsto Wilson',
+    dangerLevel: 130,
+}
+
+console.log(genericFunctionArrow<Villain>(deadPool).dangerLevel)
